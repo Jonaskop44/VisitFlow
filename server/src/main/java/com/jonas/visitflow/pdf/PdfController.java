8b33369fd1814837dda7edbc92f91fe47ca0bffe
Jonaskop44 <jonas@codeflexx.com>
2025-07-28 18:45:48 +0200

    add upload pdf

--- server/src/main/java/com/jonas/visitflow/pdf/PdfController.java
+++ server/src/main/java/com/jonas/visitflow/pdf/PdfController.java
@@ -1,4 +1,27 @@
 package com.jonas.visitflow.pdf;
 
+import com.jonas.visitflow.pdf.dto.PdfDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/pdf")
+@RequiredArgsConstructor
 public class PdfController {
+
+    private final PdfService pdfService;
+
+    @PostMapping("/{invoiceId}/upload")
+    public ResponseEntity<PdfDto> uploadPdf(@RequestParam("file") MultipartFile file,
+                                            @PathVariable Long invoiceId,
+                                            Principal principal) {
+        String userId = principal.getName();
+        PdfDto pdfDto = pdfService.uploadPdf(file, invoiceId, userId);
+        return ResponseEntity.ok(pdfDto);
+    }
+
 }
--- server/src/main/java/com/jonas/visitflow/pdf/PdfService.java
+++ server/src/main/java/com/jonas/visitflow/pdf/PdfService.java
@@ -1,16 +1,62 @@
 package com.jonas.visitflow.pdf;
 
+import com.jonas.visitflow.exception.NotFoundException;
+import com.jonas.visitflow.exception.UnauthorizedException;
+import com.jonas.visitflow.model.Invoice;
+import com.jonas.visitflow.model.PdfDocument;
 import com.jonas.visitflow.pdf.dto.PdfDto;
+import com.jonas.visitflow.repository.InvoiceRepository;
 import com.jonas.visitflow.repository.PdfRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class PdfService {
 
     private final PdfRepository pdfRepository;
+    private final InvoiceRepository invoiceRepository;
 
+    @Value("${storage.directory}")
+    private String storageDirectory;
+
+    public PdfDto uploadPdf(MultipartFile file, Long invoiceId, String userId) {
+        if(file.isEmpty() || !Objects.requireNonNull(file.getOriginalFilename()).endsWith(".pdf")) {
+            throw new IllegalArgumentException("Invalid file format");
+        }
+
+        Invoice invoice = invoiceRepository.findById(invoiceId)
+                .orElseThrow(() -> new NotFoundException("Invoice not found"));
+
+        if(!invoice.getOrder().getCompany().getUserId().equals(userId)) {
+            throw new UnauthorizedException("You are not authorized to upload a PDF for this invoice");
+        }
+
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        try {
+            Path path = Paths.get(storageDirectory).resolve(fileName);
+            Files.createDirectories(path.getParent());
+            Files.write(path, file.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to save PDF file: " + e.getMessage());
+        }
+
+        PdfDocument pdfDocument = PdfDocument.builder()
+                .fileName(fileName)
+                .invoice(invoice)
+                .build();
+        pdfDocument = pdfRepository.save(pdfDocument);
+        return PdfDto.fromEntity(pdfDocument);
+    }
 
 }
